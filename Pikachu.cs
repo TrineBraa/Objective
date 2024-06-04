0290db8c5ba5868ce3d0f86da74209926d81f9a6
trinebra <157688079+trinebra@users.noreply.github.com>
2024-06-04 12:57:31 +0200

    Add project files.

---
+++ Pikachu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objective
+{
+    //This class is basically an object that gives Pikachu the stats we set within the object.
+    internal class Pikachu
+    {
+        public int Health = 50;
+        public int Level = 5;
+
+
+        //This one will show the different aspects of the object pikachu that we have listed below. 
+        public void ShowPikachu ()
+        {
+            Console.WriteLine("\nPikachu:");
+            Console.WriteLine($"\tHealth: {Health}");
+            Console.WriteLine($"\tLevel: {Level}");
+        }
+        
+        
+
+
+    }
+
+    internal class Pokemon
+    {
+        //this object holds these values, they aren't set to anything yet. as public the outside can reach the different parts of the object,
+        //but private set means that the different parts can only be changed within the class and not by the program itself.
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Level { get; set; }
+
+
+        //The constructor takes in the different values and set them as the value of the new pokemon created.
+        public Pokemon(string name, int health, int level)
+        {
+            Name = name;
+            Health = health;
+            Level = level;
+        }
+
+
+
+        public void ShowPokemon()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Name);
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Level: {Level}");
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
+using Objective;
+
+Main();
+void Main()
+{
+    //this makes a new object of the object we have in the pikachu class
+    Pikachu pikachu = new Pikachu();
+
+
+    Console.WriteLine();
+    Console.WriteLine("A look into the more Objective related coding in C#");
+    Console.WriteLine("Working on objective oriented coding and learn that in a more proper way.");
+
+
+    //var pikachu = new
+    //{
+    //    Health = 50,
+    //    Level = 5, 
+    //};
+
+    //Console.WriteLine($"" + $"Pikachu has \n{pikachu.Health} hp and \nis level {pikachu.Level}");
+
+    //This will call on the object and the show pikachu will show the stats of pikachu object.
+    pikachu.ShowPikachu();
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("This are created by the use of the new constructor that was made!");
+    var Arcanine = new Pokemon("Arcanine", 55, 12);
+    var Zapdos = new Pokemon("Zapdos", 100, 50);
+
+    Arcanine.ShowPokemon();
+    Zapdos.ShowPokemon();
+}
+
+
+
+
+
